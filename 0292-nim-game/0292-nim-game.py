36d2bde5b658c14982e35a64f1ae7dc87d376a26
Sahithi Gurrapu <140267539+sahithi-gurrapu24@users.noreply.github.com>
2025-09-17 23:43:14 +0530

    Time: 0 ms (100%), Space: 17.5 MB (99.82%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        if n<3:
+            return True
+        if n%4 !=0:
+            return True
+        else:
+            return False
+
+        
+        
